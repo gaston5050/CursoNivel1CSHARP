80e94686191893efdc2b0177f0dadad482ce93eb
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2023-03-16 23:13:15 -0300

    segundo commit

--- Program.cs
+++ Program.cs
@@ -3,11 +3,7 @@ class Program
 {
     static void Main(string[] args)
     {
-
-
-
-        Console.WriteLine("Hello, Gastonio!");
-
+        
 
 
 
---
+++ calculadora/calculadora2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace calculadora2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n1, n2;
+         
+        Console.WriteLine("INGRESE UN NUMERO 1");
+        n1 = int.Parse(Console.ReadLine());
+          Console.WriteLine("INGRESE UN NUMERO 2");
+        n2 = int.Parse(Console.ReadLine());
+      Console.WriteLine(n1+n2);
+    }
+}
